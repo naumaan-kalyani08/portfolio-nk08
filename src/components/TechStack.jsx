a017e5142d2d41be218f14479f9b869691c9780b
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2026-01-13 23:15:30 +0530

    naming updated

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,7 +1,7 @@
 import { useLayoutEffect, useRef } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-import "../style/techStack.css";
+import "../style/TechStack.css";
 
 gsap.registerPlugin(ScrollTrigger);
 
